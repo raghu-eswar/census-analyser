14f857ca1649176c3bd1deb9127a347fbd29344f
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-05-11 09:23:51 +0530

    Added feature to load CSV files data

--- src/main/java/com/india/StateCode.java
+++ src/main/java/com/india/StateCode.java
@@ -1,5 +1,39 @@
 package com.india;
 
-public class StateCode {
+import com.opencsv.bean.CsvBindByName;
 
+public class StateCode implements Comparable{
+    @CsvBindByName(column = "State" , required = true)
+    private String stateName;
+    @CsvBindByName(column = "TIN" , required = true)
+    private int TIN;
+    @CsvBindByName(column = "StateCode" , required = true)
+    private String stateCode;
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public int getTIN() {
+        return TIN;
+    }
+
+    public String getStateCode() {
+        return stateCode;
+    }
+
+    @Override
+    public String toString() {
+        return "StateCode{" +
+                "stateName='" + stateName + '\'' +
+                ", TIN=" + TIN +
+                ", stateCode='" + stateCode + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        StateCode that = (StateCode) o;
+        return this.stateCode.compareTo(that.stateCode);
+    }
 }
--- src/test/java/com/india/census/analyzer/StateCensusAnalyserTest.java
+++ src/test/java/com/india/census/analyzer/StateCensusAnalyserTest.java
@@ -1,5 +1,55 @@
 package com.india.census.analyzer;
 
-public class StateCensusAnalyserTest {
+import com.opencsv.exceptions.CsvRuntimeException;
+import org.junit.Assert;
+import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.NoSuchFileException;
+
+public class StateCensusAnalyserTest {
+    @Test
+    public void check_number_records_loaded() throws IOException {
+            StateCensusAnalyser analyser = new StateCensusAnalyser("./stateCensusData.csv", "./stateCode.csv");
+            Assert.assertEquals(29, analyser.getStateCensusMap().size());
+            Assert.assertEquals(37, analyser.getStateCodeMap().size());
+    }
+
+    @Test
+    public void for_wrong_file_name_should_throw_exception() {
+        try {
+            new StateCensusAnalyser("./stateCensuata.csv","./stateCode.csv");
+        } catch (Exception e) {
+            Assert.assertEquals(NoSuchFileException.class, e.getClass());
+        }
+    }
+
+    @Test
+    public void for_wrong_file_extension_should_throw_exception() {
+        try {
+            new StateCensusAnalyser("./stateCensusData.cv","./stateCode.csv");
+        } catch (Exception e) {
+            Assert.assertEquals(NoSuchFileException.class, e.getClass());
+        }
+    }
+
+    @Test
+    public void for_wrong_delimiter_should_throw_exception() {
+        try {
+            new StateCensusAnalyser("./stateCensusData.csv","./stateCode.csv");
+        } catch (Exception e) {
+            Assert.assertEquals(CsvRuntimeException.class, e.getClass());
+            Assert.assertEquals("Number of data fields does not match number of headers.", e.getLocalizedMessage());
+        }
+    }
+
+    @Test
+    public void for_wrong_header_should_throw_exception() {
+        try {
+            new StateCensusAnalyser("./stateCensusData.csv","./stateCode.csv");
+        } catch (Exception e) {
+            Assert.assertEquals(CsvRuntimeException.class, e.getClass());
+            Assert.assertEquals("Error capturing CSV header!", e.getLocalizedMessage());
+        }
+    }
 }
