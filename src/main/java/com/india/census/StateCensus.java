67190677d6bd7d84e2e77d906e1623db6a37ceac
raghueswar55 <63345197+raghueswar55@users.noreply.github.com>
2020-05-11 09:24:49 +0530

    Added feature to load CSV files data

--- src/main/java/com/india/census/StateCensus.java
+++ src/main/java/com/india/census/StateCensus.java
@@ -1,5 +1,52 @@
 package com.india.census;
 
-public class StateCensus {
+import com.opencsv.bean.CsvBindByName;
 
+public class StateCensus implements Comparable {
+    @CsvBindByName(column = "State" , required = true)
+    private String stateName;
+    @CsvBindByName(column = "Population", required = true)
+    private long population;
+    @CsvBindByName(column = "AreaInSqKm", required = true)
+    private long areaInSqKm;
+    @CsvBindByName(column = "DensityPerSqKm", required = true)
+    private int densityPerSqKm;
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public long getPopulation() {
+        return population;
+    }
+
+    public long getAreaInSqKm() {
+        return areaInSqKm;
+    }
+
+    public int getDensityPerSqKm() {
+        return densityPerSqKm;
+    }
+
+    @Override
+    public String toString() {
+        return "StateCensus{" +
+                "stateName='" + stateName + '\'' +
+                ", population=" + population +
+                ", areaInSqKm=" + areaInSqKm +
+                ", densityPerSqKm=" + densityPerSqKm +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        StateCensus that = (StateCensus) o;
+        return (this.population > that.population);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        StateCensus that = (StateCensus) o;
+        return (int) (this.population - that.population);
+    }
 }
